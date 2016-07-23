00bd081a979506ec9fe973d6559c27698a9495f5
Michał Nowacki <mnowacki@neoteric.eu>
2016-07-23 14:59:04 +0200

    multiply fix

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 var main = function (){
-	console.log('gotowy do boju!');
-	var a, b;
+    console.log('gotowy do boju!');
+    var a, b
+    $('#sum').click(function(){
+    	a=parseNumber('#number_a');
+    	b=parseNumber('#number_b');
 
-	$('#sum').click(function(){
-		a = parseNumber('#number_a');
-		b = parseNumber('#number_b');
-		
-		var sum = add(a,b);
-		printScore(sum);
+    	var sum = add(a,b);
+    	printScore(sum);
+    });
+
+    $('#sub').click(function(){
+    	a=parseNumber('#number_a');
+    	b=parseNumber('#number_b');
+
+    	var sub = substract(a,b);
+    	printScore(sub);
+    });
+
+       $('#mul').click(function(){
+    	a=parseNumber('#number_a');
+    	b=parseNumber('#number_b');
+
+    	var mul = multiply(a,b);
+    	printScore(mul);
+    });
+
+	 $('#div').click(function(){
+	    	a=parseNumber('#number_a');
+	    	b=parseNumber('#number_b');
+
+	    	var div = divide(a,b);
+	    	printScore(div);
 	});
+
 }
 
 function parseNumber (id){
@@ -18,24 +42,43 @@ function parseNumber (id){
 	return number;
 }
 
+
+
 function checkCommas(temp){
-	if(temp.indexOf(',') > -1){
-		temp.replace(',', '.');
-	}
-	return temp;
+    if(temp.indexOf(',') > -1){
+        temp.replace(',', '.');
+    }
+    return temp;
 }
 
 function add(a,b){
-	var c = a + b;
+	var c= a+b;
 	return c;
 }
+
+
+function substract(a,b){
+	var c= a-b;
+	return c;
+}
+
+function multiply(a,b){
+	var c= a*b;
+	return c;
+}
+
+function divide(a,b){
+	var c= a/b;
+	if(b==0){
+		return 'Nie dzieli się przez 0!';
+	}
+	return c;
+}
+
 
 function printScore(score){
 	$('#score_indicator').text(score);
 }
-
-
-
 
 
 
